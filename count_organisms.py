9c51b6f7eb4d3b8d48bda81ecd715d0459fa91d7
git.name <operon@git.email>
2022-05-29 11:58:33 +0000

    counter working fine

--- count_organisms.py
+++ count_organisms.py
@@ -1,4 +1,7 @@
 from helpers import species_list, curl_output, string_id_n_refseq_pairs, stringdb_aliases
+from threading import get_ident
+from collections import defaultdict
+from queue import SimpleQueue, Empty
 import requests
 from itertools import chain, tee, groupby
 from time import sleep
@@ -10,7 +13,12 @@ import pickle
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
-data = {}
+data_path = Path('count_organisms.pkl')
+if data_path.exists():
+    with open(data_path) as f:
+        data = pickle.load(f)
+else:
+    data = {}
 count = 0
 
 def pairwise(iterable):
@@ -19,16 +27,15 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
-session = requests.Session()
+sessions = defaultdict(lambda: requests.Session())
 
-def f(organism_selection, genome_organism_id):
+def f(genome_organism_id, organism_selection):
     if organism_selection in data:
         return
     try:
-        print(f"[{genome_organism_id}]")
         string_refseq_gen = chain(string_id_n_refseq_pairs(genome_organism_id), pairwise(k for k, _ in groupby(m.groups()[0] for m in re.finditer(r"^\d+\.(\S*)\t.*$", stringdb_aliases(genome_organism_id), re.MULTILINE))))
         for _  in range(3):
-            resp = session.post(
+            resp = sessions[get_ident()].post(
                 'https://patricbrc.org/api/genome_feature',
                 headers={'Content-Type': 'application/x-www-form-urlencoded'},
                 data=f"and(keyword(%22{genome_organism_id}%22),or({','.join(['keyword(%22' + a_string_id + '%22),keyword(%22' + a_refseq + '%22)' for _, (a_string_id, a_refseq) in zip(range(20), string_refseq_gen)])}))&limit(1)"
@@ -40,9 +47,9 @@ def f(organism_selection, genome_organism_id):
                     print(features)
                 genome_id = features[0]['genome_id']
                 data.get(organism_selection, set()).add(genome_id)
+                data.get("errors", {}).pop(organism_selection, None)
                 global count
                 count += 1
-                print(count)
                 break
         else:
             print("Couldn't find", organism_selection, genome_organism_id, file=sys.stderr)
@@ -51,15 +58,18 @@ def f(organism_selection, genome_organism_id):
         data.get("errors", {})[organism_selection] = str(e)
         raise
 
-for i, s in species_list():
-    f(s, i )
-#with ThreadPoolExecutor() as ex:
-#    sl = species_list()
-#    for i, r in enumerate(as_completed([ex.submit(s) for s in sl])):
-#        r.result()
-#        print(round((i+1)/len(sl), 1), end='\r')
-#
-#with open('count_organisms.pkl', 'wb') as file:
-#    pickle.dump(data, file)
-#print("errors", data.get("errors"))
-#print("Total count", count)
+sl = species_list()
+sync = 0
+if sync:
+    for s in sl:
+        f(*s)
+else:
+    with ThreadPoolExecutor() as ex:
+        for i, r in enumerate(as_completed([ex.submit(f, *s) for s in sl])):
+            r.result()
+            print(round((i+1)/len(sl), 1), end='\r')
+
+with open(data_path, 'wb') as file:
+    pickle.dump(data, file)
+print("errors", data.get("errors"))
+print("Total count", count)
